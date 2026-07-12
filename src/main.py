770c3710adf1f7ea01f58f3f1b0c9ddc0c2d8907
Moiz-205 <moizrajput205@gmail.com>
2026-07-12 16:32:45 +0500

    new command added for updating dotfile with url

--- src/main.py
+++ src/main.py
@@ -1,17 +1,27 @@
 import typer
 from core.initializer import init_project
-
+from core.assigner import assign_url
 
 app = typer.Typer()
 
 @app.command()
 def init(name: str = typer.Argument(None)):
     try:
-        init_project(name)
-        typer.echo(f"✓ Initialized project at {name or '.'}")
+        path = init_project(name)
+        typer.echo(f"✓ Initialized project at {path}")
     except FileExistsError as e:
-        typer.echo(f"● {e}", err=True)
+        typer.echo(f"✗ {e}", err=True)
         raise typer.Exit(code=1)
+
+@app.command()
+def set(url: str):
+    try:
+        path = assign_url(url)
+        typer.echo(f"✓ URL saved to {path.name}")
+    except FileNotFoundError as e:
+        typer.echo(f"✗ {e}", err=True)
+        raise typer.Exit(code=1)
+
 
 if __name__ == "__main__":
     app()
